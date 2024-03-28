e04d417cad5d2e5bc1f146de1db5a83eeade408b
Abdillah-MD <ambdi6@gmail.com>
2024-03-28 19:52:37 +0100

    react router dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './index.css'
+import Header from './components/Header/main.jsx'
+import Home from './pages/Home/main.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/accueil' element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
