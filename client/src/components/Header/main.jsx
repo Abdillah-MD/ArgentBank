797102508ab521e9bf56edbe6b8394bd8049e294
Abdillah-MD <ambdi6@gmail.com>
2024-03-29 14:28:35 +0100

    Url de navigation du header

--- client/src/components/Header/main.jsx
+++ client/src/components/Header/main.jsx
@@ -1,8 +1,10 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import logo from "/images/argentBankLogo.png"
 import './style.scss'
 
 const Header = () => {
+    const location = useLocation()
+
     return (
         <header className="header">
             <Link className="header_logo" to="/accueil" >
@@ -10,8 +12,16 @@ const Header = () => {
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <nav className="header_nav">
-                <Link className="header_nav-link" to="sign-in"><i className="fa-solid fa-circle-user"></i> Sign in</Link>
-                <Link className="header_nav-link" to="/accueil"><i className="fa-solid fa-right-from-bracket"></i> Sign out</Link>
+
+                {/* Regarder l'url pour afficher les liens de navigation */}
+                
+                {location.pathname === "/admin" || location.pathname === "/admin/user"
+                    ? <>
+                        <Link className="header_nav-link" to="/admin/user"><i className="fa-solid fa-circle-user"></i> Name</Link>
+                        <Link className="header_nav-link" to="/accueil"><i className="fa-solid fa-right-from-bracket"></i> Sign out</Link>
+                    </> 
+                    : <Link className="header_nav-link" to="sign-in"><i className="fa-solid fa-circle-user"></i> Sign in</Link>
+                }
             </nav>
         </header>
     )
