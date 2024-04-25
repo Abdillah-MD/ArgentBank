c226c83a4f76be666378f4381e34581d472f90ec
Abdillah-MD <ambdi6@gmail.com>
2024-04-25 09:45:47 +0200

    modification condition affichage user connecté

--- client/src/components/Header/main.jsx
+++ client/src/components/Header/main.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom"
+import { Link } from "react-router-dom"
 import logo from "/images/argentBankLogo.webp"
 import './style.scss'
 import { useDispatch, useSelector } from "react-redux"
@@ -8,7 +8,7 @@ import { useEffect } from "react"
 import { getUser, logout } from "../../redux/slices/userSlice"
 
 const Header = () => {
-    const location = useLocation()
+    // const location = useLocation()
     const dispatch = useDispatch()
 
     // Récupérer l'état Redux
@@ -30,24 +30,23 @@ const Header = () => {
 
     return (
         <header className="header">
-            <Link className="header_logo" to="/accueil" onClick={removeToken}>
+            <Link className="header_logo" to="/accueil">
                 <img src={logo} alt="Argent Bank" />
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <nav className="header_nav">
 
                 {/* Regarder l'url pour afficher les liens de navigation */}
-                
-                {location.pathname === "/admin" || location.pathname === "/admin/user"
-                    ? <>
-                    {isAuthenticated && (
+                {
+                    isAuthenticated === true ?
                         <>
-                            <p className="header_nav-link" to="/admin/"><i className="fa-solid fa-circle-user"></i> <UserName name={`${User?.userName}`} /></p>
+                            <Link className="header_nav-link" to="/admin"><i className="fa-solid fa-circle-user"></i> &nbsp;<UserName name={`${User?.userName}`} /></Link>
                             <Link className="header_nav-link" to="/accueil" onClick={removeToken}><i className="fa-solid fa-right-from-bracket"></i>&nbsp; Sign out</Link>
                         </>
-                    )}
-                </>  
-                    : <Link className="header_nav-link" to="sign-in"><i className="fa-solid fa-circle-user"></i>&nbsp; Sign in</Link>
+                    :<>
+                        <Link className="header_nav-link" to="sign-in"><i className="fa-solid fa-circle-user"></i>&nbsp; Sign in</Link>
+                    </>
+
                 }
             </nav>
         </header>
